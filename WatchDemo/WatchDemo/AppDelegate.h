246b3b047959a84b5b2f13019695aa13ebf115c7
Rob Nasby <rob.nasby@mcmaster.com>
2015-05-14 14:56:41 -0500

    Add an application to demo Apple Watch functionality.

---
+++ WatchDemo/WatchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WatchDemo
+//
+//  Created by Robert Nasby on 5/14/15.
+//  Copyright (c) 2015 McMaster-Carr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WatchDemo/WatchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatchDemo
+//
+//  Created by Robert Nasby on 5/14/15.
+//  Copyright (c) 2015 McMaster-Carr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
