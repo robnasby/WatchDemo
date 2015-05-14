bad9d1143405d1277fd76c75ec14445b28505534
Rob Nasby <rob.nasby@mcmaster.com>
2015-05-14 16:50:09 -0500

    Add status messages text area.

--- WatchDemo/WatchDemo/ViewController.m
+++ WatchDemo/WatchDemo/ViewController.m
@@ -8,21 +8,48 @@
 
 #import "ViewController.h"
 
+#define STATUS_MESSAGES_TEXTVIEW_TAG 21
+
 @interface ViewController ()
+
+@property(nonatomic, weak) UITextView *statusMessagesTextView;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - View Lifecycle
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    
+    self.statusMessagesTextView = (UITextView *)[self.view viewWithTag:STATUS_MESSAGES_TEXTVIEW_TAG];
+    self.statusMessagesTextView.layer.borderColor = [[UIColor grayColor] CGColor];
+    self.statusMessagesTextView.layer.borderWidth = 1;
+}
+
+
 #pragma mark - Action Outlets
 
 - (IBAction)sendOrderConfirmationNotification_TouchUpInside:(id)sender {
+    [self appendStatusMessage:@"Sending an order confirmation notification..."];
 }
 
 - (IBAction)sendPackageDeliveryNotification_TouchUpInside:(id)sender {
+    [self appendStatusMessage:@"Sending a package delivery notification..."];
 }
 
 - (IBAction)simulateRapidReordering_TouchUpInside:(id)sender {
+    [self appendStatusMessage:@"Simulating a rapid reordering interaction..."];
+}
+
+
+#pragma mark - Status Message Helpers
+
+- (void)appendStatusMessage:(NSString *)message {
+    self.statusMessagesTextView.text = [NSString stringWithFormat:@"%@\n%@",
+                                        message,
+                                        self.statusMessagesTextView.text];
 }
 
 @end
