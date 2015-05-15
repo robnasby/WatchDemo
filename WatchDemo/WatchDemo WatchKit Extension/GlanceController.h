600ad1d66c5a8839435e9a35249872545088784d
Rob Nasby <rob.nasby@mcmaster.com>
2015-05-15 16:01:03 -0500

    Add WatchKit app and extension to the watch demo application.

---
+++ WatchDemo/WatchDemo WatchKit Extension/GlanceController.h
@@ -0,0 +1,14 @@
+//
+//  GlanceController.h
+//  WatchDemo WatchKit Extension
+//
+//  Created by Robert Nasby on 5/15/15.
+//  Copyright (c) 2015 McMaster-Carr. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface GlanceController : WKInterfaceController
+
+@end
