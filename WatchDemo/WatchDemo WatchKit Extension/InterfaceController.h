e41a9be2f58aab0c1a5a875bd88defd2dcd7f625
Rob Nasby <rob@nasbys.com>
2015-05-17 00:21:25 -0500

    Add an application to demo the Apple Watch.

---
+++ WatchDemo/WatchDemo WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  WatchDemo WatchKit Extension
+//
+//  Created by Robert Nasby on 5/18/15.
+//  Copyright (c) 2015 McMaster-Carr. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ WatchDemo/WatchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatchDemo
+//
+//  Created by Rob Nasby on 5/17/15.
+//  Copyright (c) 2015 McMaster-Carr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
