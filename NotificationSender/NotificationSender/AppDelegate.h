02ff6f282f96d967ef299c94c423acda53c5a3f8
Rob Nasby <rob@nasbys.com>
2015-05-16 23:12:15 -0500

    Add an application to send remote notifications.

---
+++ NotificationSender/NotificationSender/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NotificationSender
+//
+//  Created by Rob Nasby on 5/16/15.
+//  Copyright (c) 2015 McMaster-Carr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NotificationSender/NotificationSender/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NotificationSender
+//
+//  Created by Rob Nasby on 5/16/15.
+//  Copyright (c) 2015 McMaster-Carr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
