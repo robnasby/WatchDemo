4cce22b3c602bfb76289e68c1d7ecc10d270645b
Rob Nasby <rob@nasbys.com>
2015-05-18 08:45:17 -0500

    Add support for sending remote notification to watch demo app.

--- NotificationSender/NotificationSender/ViewController.m
+++ NotificationSender/NotificationSender/ViewController.m
@@ -6,19 +6,41 @@
 //  Copyright (c) 2015 McMaster-Carr. All rights reserved.
 //
 
+#import "NWPusher.h"
+#import "NWSecTools.h"
+#import "NWSSLConnection.h"
 #import "ViewController.h"
+
+#define APNS_DEVELOPMENT_CERTIFICATE_PASSWORD @"McMaster1901"
 
 #define STATUS_MESSAGES_TEXT_VIEW_TAG 22
 #define TOKEN_TEXT_VIEW_TAG 12
 
 @interface ViewController ()
 
+@property (strong, nonatomic) NWPusher *pusher;
 @property (weak, nonatomic) UITextView *statusMessagesTextView;
+@property (strong, nonatomic, readonly) NSString *token;
 @property (weak, nonatomic) UITextView *tokenTextView;
 
 @end
 
 @implementation ViewController
+
+#pragma mark - Properties
+
+- (NSString *)token
+{
+    NSString *token = self.tokenTextView.text;
+    
+    if (token.length == 0) return nil;
+
+    token = [token stringByTrimmingCharactersInSet: [NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+    token = [token stringByReplacingOccurrencesOfString:@" " withString:@""];
+    
+    return token;
+}
+
 
 #pragma mark - View Lifecycle
 
@@ -53,11 +75,95 @@
 
 - (IBAction)sendOrderConfirmationButton_TouchUpInside:(id)sender {
     [self appendStatusMessage:@"Sending order confirmation notification..."];
+
+    NSString *token = self.token;
+    if (token) {
+        [self sendNotificationWithBody:@"We have received your order of 2 electrical enclosures."
+                               toToken:token];
+    }
 }
 
 - (IBAction)sendPackageDeliveryButton_TouchUpInside:(id)sender {
     [self appendStatusMessage:@"Sending package delivery notification..."];
+    
+    NSString *token = self.token;
+    if (token) {
+        [self sendNotificationWithBody:@"We have delivered your order of paint brushes and wing nuts to Joanna."
+                               toToken:token];
+    }
 }
+
+
+#pragma mark - Remote Notification Helpers
+
+- (void)checkStatusUsingPusher:(NWPusher *)pusher
+{
+    NSUInteger identifier = 0;
+    NSError *apnError = nil;
+    NSError *error = nil;
+    BOOL read = [pusher readFailedIdentifier:&identifier apnError:&apnError error:&error];
+    if (read && apnError) {
+        NSLog(@"Notification with identifier %i rejected: %@", (int)identifier, apnError);
+    } else if (error) {
+        NSLog(@"Unable to read failed: %@", error);
+    }
+}
+
+- (NWPusher *)createPusher
+{
+    NSURL *url = [NSBundle.mainBundle URLForResource:@"WatchDemo - Push Notification - Development.p12" withExtension:nil];
+    NSData *pkcs12 = [NSData dataWithContentsOfURL:url];
+    NSError *error = nil;
+    NWPusher *pusher = [NWPusher connectWithPKCS12Data:pkcs12 password:APNS_DEVELOPMENT_CERTIFICATE_PASSWORD error:&error];
+    if (pusher) {
+        NWCertificateRef certificate = [NWSecTools certificateWithIdentity:pusher.connection.identity error:&error];
+        NSLog(@"Connected to APN: %@%@",
+              [NWSecTools summaryWithCertificate:certificate],
+              [NWSecTools isSandboxCertificate:certificate] ? @" (sandbox)" : @"");
+    } else {
+        NSLog(@"Unable to connect: %@", error);
+    }
+    
+    return pusher;
+}
+
+- (void)sendNotificationWithBody:(NSString *)body
+                         toToken:(NSString *)token
+{
+    NWPusher *pusher = [self createPusher];
+
+    NSDictionary *payload = @{@"aps": @{@"alert": body, @"sound": @"default"}};
+    NSString *jsonPayload = [self serializeApnPayload:payload];
+    
+    NSError *error = nil;
+    BOOL pushed = [pusher pushPayload:jsonPayload token:token identifier:rand() error:&error];
+    if (pushed) {
+        NSLog(@"Pushed to APNs");
+    } else {
+        NSLog(@"Unable to push: %@", error);
+    }
+    
+    [self performSelector:@selector(checkStatusUsingPusher:) withObject:pusher afterDelay:2.0];
+}
+
+- (NSString *)serializeApnPayload:(NSDictionary *)payload
+{
+    NSString *jsonString = nil;
+    
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:payload
+                                                       options:NSJSONWritingPrettyPrinted // Pass 0 if you don't care about the readability of the generated string
+                                                         error:&error];
+    
+    if (! jsonData) {
+        NSLog(@"Got an error: %@", error);
+    } else {
+        jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    }
+    
+    return jsonString;
+}
+
 
 #pragma mark - Status Message Helpers
 
