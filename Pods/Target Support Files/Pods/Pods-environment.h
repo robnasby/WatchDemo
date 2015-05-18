f0d430ef1394131410f81c706f6181d8becf8416
Rob Nasby <rob@nasbys.com>
2015-05-17 22:28:30 -0500

    Add NWPusher CocoaPods.

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// NWPusher
+#define COCOAPODS_POD_AVAILABLE_NWPusher
+#define COCOAPODS_VERSION_MAJOR_NWPusher 0
+#define COCOAPODS_VERSION_MINOR_NWPusher 6
+#define COCOAPODS_VERSION_PATCH_NWPusher 3
+
